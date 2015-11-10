00f364e5f4e84d5973b73815cdfe795bf147a219
peter <pkeoman17@gmail.com>
2015-11-09 16:16:26 -0800

    front end sign in

--- user/index.php
+++ user/index.php
@@ -3,4 +3,15 @@ require("../inc/header.inc");
 ?>
 <h1> Sign in </h1>
 
+<form id='login' action='login.php' method='post' accept-charset='UTF-8'>
+
+	<input type='hidden' name='submitted' id='submitted' value='1'/>
+	<div class="form-group">
+		<div class="col-sm-3">
+			<label>Handle: </label>
+			<input type='text' name='handle' class="form-control" id='user_handle' values='<?= htmlentities(@_POST['handle']) ?>'/>
+		</div>
+	</div>
+</form>
+
 
