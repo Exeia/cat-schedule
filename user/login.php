7738228c6d08ea1481665d001eb964a22232032c
peter <pkeoman17@gmail.com>
2015-11-10 15:22:15 -0800

    working backend user

--- user/login.php
+++ user/login.php
@@ -1,3 +1,33 @@
 <?php
+include("../inc/database.php");
 
+$handle = $_POST['handle'];
+
+$dog =get_dog($handle);
+
+if(sizeof($dog) == 1)
+{
+//redirect to home screen with buttons
+	session_register("dog");
+	header("location:../index.php");
+}
+else if(sizeof($dog)> 1 )
+{
+//redirect to this page
+
+	?>
+	<ul>
+		<?foreach($dog as $i){
+
+			echo  "<tr> <a href='login.php'>$i</a> </tr>";
+
+		}
+		?>
+		</ul>
+
+}
+else
+{
+//redirect
+}
 ?>
